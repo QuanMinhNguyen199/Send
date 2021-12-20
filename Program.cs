4aac43e8dc58dd46e8a1fb877c2218ccf0b79d84
minhq <minhq@MSI>
2021-12-20 23:48:09 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "SampleK", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                string message = "I'm from the host queue";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "SampleK", basicProperties: null, body:body);
+                Console.WriteLine("Sent {0}",message);
+            }
+            Console.WriteLine("Press [Enter] to exit !");
+            Console.ReadLine();
+
+        }
+    }
+}
